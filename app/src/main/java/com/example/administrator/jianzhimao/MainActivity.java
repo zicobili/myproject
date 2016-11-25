01f820851d3e16f8898e6b6f8b06afbd888804a2
1505737818@qq.com <1505737818@qq.com>
2016-11-25 10:45:48 +0800

    添加主导航，调整item样式

--- app/src/main/java/com/example/administrator/jianzhimao/MainActivity.java
+++ app/src/main/java/com/example/administrator/jianzhimao/MainActivity.java
@@ -4,71 +4,79 @@ package com.example.administrator.jianzhimao;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
-    private RadioGroup rg_tab_bar;
-    private RadioButton rb_home;
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+    private RadioGroup navbar;
+    private RadioButton rb1;
 
     private FragmentIndex fg1;
     private FragmentAlljob fg2;
     private FragmentMiao fg3;
     private FragmentMe fg4;
-    private FragmentManager fManager;
+    private FragmentManager main_fManager;
+    private FragmentTransaction main_fTrasaction;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fManager = getFragmentManager();
-        rg_tab_bar = (RadioGroup) findViewById(R.id.rg);
-        rg_tab_bar.setOnCheckedChangeListener(this);
-        //获取第一个单选按钮，并设置其为选中状态
-        rb_home = (RadioButton) findViewById(R.id.rb1);
-        rb_home.setChecked(true);
+
+        //navnar start
+        main_fManager = getFragmentManager();
+        navbar= (RadioGroup) findViewById(R.id.main_navbar);
+        navbar.setOnCheckedChangeListener(this);
+        rb1= (RadioButton) findViewById(R.id.main_rb1);
+        rb1.setChecked(true);
+
+
     }
+
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-        FragmentTransaction fTransaction = fManager.beginTransaction();
-        hideAllFragment(fTransaction);
+        main_fTrasaction = main_fManager.beginTransaction();
+        hideAllFragment(main_fTrasaction);
         switch (checkedId){
-            case R.id.rb1:
+            case R.id.main_rb1:
                 if(fg1 == null){
                     fg1 = new FragmentIndex();
-                    fTransaction.add(R.id.ly_content,fg1);
-                }else{
-                    fTransaction.show(fg1);
+                    main_fTrasaction.add(R.id.main_lycontent,fg1);
+                }else {
+                    main_fTrasaction.show(fg1);
                 }
                 break;
-            case R.id.rb2:
+            case R.id.main_rb2:
                 if(fg2 == null){
                     fg2 = new FragmentAlljob();
-                    fTransaction.add(R.id.ly_content,fg2);
-                }else{
-                    fTransaction.show(fg2);
+                    main_fTrasaction.add(R.id.main_lycontent,fg2);
+                }else {
+                    main_fTrasaction.show(fg2);
                 }
                 break;
-            case R.id.rb3:
+            case R.id.main_rb3:
                 if(fg3 == null){
                     fg3 = new FragmentMiao();
-                    fTransaction.add(R.id.ly_content,fg3);
-                }else{
-                    fTransaction.show(fg3);
+                    main_fTrasaction.add(R.id.main_lycontent,fg3);
+                }else {
+                    main_fTrasaction.show(fg3);
                 }
                 break;
-            case R.id.rb4:
+            case R.id.main_rb4:
                 if(fg4 == null){
                     fg4 = new FragmentMe();
-                    fTransaction.add(R.id.ly_content,fg4);
-                }else{
-                    fTransaction.show(fg4);
+                    main_fTrasaction.add(R.id.main_lycontent,fg4);
+                }else {
+                    main_fTrasaction.show(fg4);
                 }
                 break;
         }
-        fTransaction.commit();
+        main_fTrasaction.commit();
+
     }
-    //隐藏所有Fragment
     private void hideAllFragment(FragmentTransaction fragmentTransaction){
         if(fg1 != null)fragmentTransaction.hide(fg1);
         if(fg2 != null)fragmentTransaction.hide(fg2);
